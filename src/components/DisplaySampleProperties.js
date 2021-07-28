6872fd66d469a82889c304ec8a7adfe0a3540851
Serhii Komarychev <komarichev@gmail.com>
2021-07-28 09:26:29 +0300

    fix display sample

--- src/components/DisplaySampleProperties.js
+++ src/components/DisplaySampleProperties.js
@@ -1,23 +1,18 @@
-import React from "react";
+import React from 'react'
 
-const DisplaySampleProperties = ({ SamplePropertiesArr, mainArray }) =>
-{
-  const findPrefix=(index)=>{
-   const prefix= mainArray.reduce((acc,val,i)=>   { return( val[0]["title"]===SamplePropertiesArr[0]? val :acc)})[index]["prefix"];
-    return(prefix!==undefined?prefix:"")
-    }
+const DisplaySampleProperties = ({ SamplePropertiesArr, mainArray }) => {
+  const arr = mainArray.reduce((acc, rec) => rec[0].title === SamplePropertiesArr[0] ? rec : acc)
 
-    const finsPostfix=(index)=>{
-        const postfix= mainArray.reduce((acc,val,i)=>   { return( val[0]["title"]===SamplePropertiesArr[0]? val :acc)})[index]["postfix"];
-         return(postfix!==undefined?postfix:" ")
-         }
-  return(
-  SamplePropertiesArr.map((el,i) => (
-      <button key={el} id={i} onClick={(e)=>(console.log(e.target))}>
-    <i>
-      {findPrefix(i)+el+finsPostfix(i)} 
-     </i>
-     </button>
-  )));}
+  return SamplePropertiesArr.map((el, i) => {
+    const { prefix = '', postfix = '' } = arr[i]
+    return (
+      <button key={el} onClick={(e) => (console.log(e.target))}>
+        <i>
+          {`${prefix}${el}${postfix}`}
+        </i>
+      </button>
+    )
+  })
+}
 
-  export default DisplaySampleProperties;
+export default DisplaySampleProperties
